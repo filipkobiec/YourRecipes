ab5a7f9375a8dd03760379e7e6181525d9ad79ec
FiPiL99 <fipis14@gmail.com>
2022-04-16 14:16:05 +0200

    Add models folder with recipe model

---
+++ Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿namespace YourRecipes.Models
+{
+    public class Recipe
+    {
+        public enum CuisineType
+        {
+            None,
+            Italian,
+            Indian,
+            Mexican
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Descritption { get; set; }
+        public string Image { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
