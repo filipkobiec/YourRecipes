eaca13053b965985089ac6c5d207c4780b4d01d2
FiPiL99 <fipis14@gmail.com>
2022-04-16 14:16:22 +0200

    Add recipes list page

---
+++ Pages/Recipes/List.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YourRecipes.Data;
+using YourRecipes.Models;
+
+namespace YourRecipes.Pages.Recipes
+{
+    public class ListModel : PageModel
+    {
+        private readonly IRecipeData _recipeData;
+        public IEnumerable<Recipe> Recipes { get; set; }
+        public ListModel(IRecipeData recipeData)
+        {
+            _recipeData = recipeData;
+        }
+        public void OnGet()
+        {
+            Recipes = _recipeData.GetAll();
+        }
+    }
+}
